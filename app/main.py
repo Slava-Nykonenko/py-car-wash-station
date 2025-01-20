fd20e113d0c21345d68c6e2ca6f458c863e4717d
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-01-20 16:20:11 +0200

    Solution. Classes and methods were created. (After fixing the issues)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
-            -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> \
+            None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -14,7 +14,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> list:
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
